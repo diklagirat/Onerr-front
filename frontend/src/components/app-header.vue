6e5322a863789c244d9d7d5ebc08f3faa1a50bef
dikla <dikla.giratt@gmail.com>
2022-07-17 21:55:58 +0300

    update sass

---
+++ frontend/src/components/app-header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="app-header">
+        <h1>header</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ frontend/src/views/home-page.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="home-page">
+        <h1>home page</h1>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
